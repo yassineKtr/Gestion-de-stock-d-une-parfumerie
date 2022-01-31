c7f4d849404d984a1cbdf455411101ac58e01f24
ktiri <yassinektiri@gmail.com>
2022-01-31 23:41:30 +0100

    fixed some unexpected issues

--- Gestion de stock d'une parfumerie.Tests/ServicesTests.cs
+++ Gestion de stock d'une parfumerie.Tests/ServicesTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gestion_de_stock_d_une_parfumerie.Tests
+namespace GestionStockCDN.Tests
 {
     public class ServicesTests
     {
