8afc965036954bb902c1d4e96308e1d2b8654437
ktiri <yassinektiri@gmail.com>
2022-01-31 23:27:06 +0100

    added tests project

---
+++ Gestion de stock d'une parfumerie.Tests/ServicesTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_stock_d_une_parfumerie.Tests
+{
+    public class ServicesTests
+    {
+    }
+}
