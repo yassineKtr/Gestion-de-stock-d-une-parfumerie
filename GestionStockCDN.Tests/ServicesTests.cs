c4090e0e4f852c26daf63584691caf3a325acfc7
ktiri <yassinektiri@gmail.com>
2022-02-03 00:31:31 +0100

    Bug fixes

--- GestionStockCDN.Tests/ServicesTests.cs
+++ GestionStockCDN.Tests/ServicesTests.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using GestionStockCDN.Models;
-using GestionStockCDN.DAL;
-using Xunit;
 using GestionStockCDN.Project.Services;
+using Xunit;
 
 
 namespace GestionStockCDN.Tests
