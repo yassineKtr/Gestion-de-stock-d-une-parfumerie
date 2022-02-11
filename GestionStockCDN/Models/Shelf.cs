8b488879fed015b2fb6509faedf95a9502946947
yassine ktiri <yassinektiri@gmail.com>
2022-02-11 22:46:04 +0100

    fixed some issues

--- GestionStockCDN/Models/Shelf.cs
+++ GestionStockCDN/Models/Shelf.cs
@@ -5,18 +5,7 @@ namespace GestionStockCDN.Models
 {
     public class Shelf
     {
-
-        private String _brand;
-        private List<int> _perfumes;
-
-        public string brand { get => _brand; set => _brand = value; }
-        public List<int> perfumes { get => _perfumes; set => _perfumes = value; }
-
-
-       
-
-
-
-
+        public string brand { get; set ; }
+        public List<int> perfumes { get ; set ; }
     }
 }
