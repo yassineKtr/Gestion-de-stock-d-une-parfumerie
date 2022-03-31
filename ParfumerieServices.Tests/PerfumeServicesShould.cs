10a67fd5850c3fb37e0ffe48af038c6efa37ec39
yassine ktiri <yassinektiri@gmail.com>
2022-03-31 01:08:08 +0000

    added some tests, still need to fix others

--- ParfumerieServices.Tests/PerfumeServicesShould.cs
+++ ParfumerieServices.Tests/PerfumeServicesShould.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Moq;
+﻿using Moq;
 using ParfumerieServices.Repositories;
 using ParfumerieServices.Services;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace ParfumerieServices.Tests
@@ -13,13 +11,90 @@ namespace ParfumerieServices.Tests
     public class PerfumeServicesShould
     {
         [Fact]
-        public void ShouldAddPerfume()
+        public void GetAllPerfumes()
         {
             //Arrange
-            var service = new Mock<IPerfumeServices>();
+            var mockResults = TestMethodes.GetPerfumes();
+            var mockRepo= new Mock<IPerfumeRepository>();
+            mockRepo.Setup(x => x.GetPerfumes()).Returns(mockResults);
+            var expected = mockResults.Result.ToList();
             //Act
-           
+            var result = mockRepo.Object.GetPerfumes().Result.ToList();
+
             //Assert
+            Assert.True(result.SequenceEqual(expected));
         }
+
+        [Fact]
+        public void GetPerfumeById()
+        {
+            //Arrange
+            var mockResults = TestMethodes.GetPerfumeById();
+            var perfumeToBeRetrieved = mockResults.Result;
+            var mockRepo = new Mock<IPerfumeRepository>();
+            mockRepo.Setup(x => x.GetPerfume(perfumeToBeRetrieved.id)).Returns(mockResults);
+            var expected = perfumeToBeRetrieved;
+            //Act
+            var result = mockRepo.Object.GetPerfume(perfumeToBeRetrieved.id).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(expected.Equals(result));
+        }
+
+
+        [Fact]
+        public void GetPerfumesByBrand()
+        {
+            //Arrange
+            var mockResults = TestMethodes.GetPerfumesOfASingleBrand();
+
+            var mockRepo = new Mock<IPerfumeServices>();
+            mockRepo.Setup(x => x.GetPerfumesByBrand("brand1")).Returns(mockResults);
+            var expected = mockResults.Result;
+            //Act
+            var result = mockRepo.Object.GetPerfumesByBrand("brand1").Result;
+
+            //Assert
+            Assert.True(expected.Equals(result));
+        }
+
+        [Fact]
+        public void GetAllBrands()
+        {
+            //Arrange
+            var mockResults = TestMethodes.GetBrands();
+            
+            var mockRepo = new Mock<IPerfumeServices>();
+            mockRepo.Setup(x => x.GetAllBrands()).Returns(mockResults);
+            var expected = mockResults.Result;
+            //Act
+            var result = mockRepo.Object.GetAllBrands().Result;
+
+            //Assert
+            Assert.True(expected.Equals(result));
+        }
+
+        [Fact]
+        public  void UpdatePerfume()
+        {
+            //Arrange
+            var mockResults = TestMethodes.GetPerfumeById();
+            var perfumeToBeModified = mockResults.Result;
+            var perfumeAfterMod = perfumeToBeModified;
+            perfumeAfterMod.brand = "newBrand";
+            var mockRepo = new Mock<IPerfumeRepository>();
+            mockRepo.Setup(x => x.UpdatePerfume(perfumeToBeModified)).Returns(Task.FromResult(perfumeAfterMod));
+            var expected = mockResults;
+            //Act
+            var result = Task.FromResult(mockRepo.Object.UpdatePerfume(perfumeToBeModified));
+
+            //Assert
+            Assert.True(expected.Equals(result));
+        }
+
+        
+
+
     }
 }
